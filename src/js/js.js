61880a50c490fcaa893bd4faa4f2d75c319c2f4e
CDog <i@cdog.me>
2016-02-21 02:47:56 +0800

    Brand-New World

--- src/js/js.js
+++ src/js/js.js
@@ -1,9 +1,28 @@
 {
+    let fadeOutEle=(ele)=>{
+        ele.classList.add('fade-out-animation');
+        let afterOut=()=>{
+            ele.classList.remove('fade-out-animation');
+            ele.style.display='none';
+            ele.removeEventListener("animationend",afterOut);
+        };
+        ele.addEventListener("animationend", afterOut)
+    };
+
+    let fadeInEle=(ele,dsp)=>{
+        ele.removeAttribute('style');
+        ele.style.display=dsp;
+        ele.classList.add('fade-in-animation');
+        let afterIn=()=>{
+            ele.classList.remove('fade-in-animation');
+            ele.removeEventListener("animationend",afterIn);
+        };
+        ele.addEventListener("animationend", afterIn)
+    };
+
+
     window.onload=()=>{
         let loader=document.getElementsByClassName('loader')[0]
-        loader.classList.add('fade-animation');
-        loader.addEventListener("animationend", ()=>{
-            loader.style.display='none';
-        })
+        fadeOutEle(loader);
     }
 }
