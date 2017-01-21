3f199bf7f0c6a477708dbbe07849559aa51b7eac
Cao Junquan <i@cdog.me>
2017-01-21 12:31:39 +0800

    Remove eggache scripts.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,36 +11,36 @@ import htmlminify from 'gulp-html-minify';
 import 'babel-polyfill';
 
 
-let base={
-    src:'./src/',
-    dest:'build/'
+let base = {
+    src: './src/',
+    dest: 'build/'
 };
 
-let styleBase={
-    src:base.src+'/style/*.css',
-    devDest:".tmp/style",
-    buildDest:base.dest
+let styleBase = {
+    src: base.src + '/style/*.scss',
+    devDest: ".tmp/style",
+    buildDest: base.dest
 };
 
-let scriptBase={
-    src:base.src+'/js/js.js',
-    devDest:".tmp/js",
-    buildDest:base.dest
+let scriptBase = {
+    src: base.src + '/js/js.js',
+    devDest: ".tmp/js",
+    buildDest: base.dest
 };
 
-gulp.task('dev-styles',()=>{
+gulp.task('dev-styles', () => {
     return gulp.src(styleBase.src)
         .pipe(plumber())
         .pipe(sass())
         .pipe(autoprefix({
-            browsers:['> 1%','last 4 Android versions']
+            browsers: ['> 1%', 'last 4 Android versions']
         }))
         .pipe(gulp.dest(styleBase.devDest))
         .pipe(connect.reload());
 
 });
 
-gulp.task('dev-scripts',()=>{
+gulp.task('dev-scripts', () => {
     return gulp.src(scriptBase.src)
         .pipe(plumber())
         .pipe(babel({
@@ -50,41 +50,46 @@ gulp.task('dev-scripts',()=>{
         .pipe(gulp.dest(scriptBase.devDest))
         .pipe(connect.reload());
 
-})
+});
 
-gulp.task('dev-connect',()=>{
+gulp.task('build-scss', () => {
+    return gulp.src([styleBase.src])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(styleBase.devDest));
+});
+
+gulp.task('dev-connect', () => {
     connect.server({
-        host:"0.0.0.0",
-        port:8888,
-        livereload:true,
-        root:['.tmp/','src/']
+        host: "0.0.0.0",
+        port: 8888,
+        livereload: true,
+        root: ['.tmp/', 'src/']
     })
 });
-gulp.task('dev-watch',()=>{
-    gulp.watch(styleBase.src,['dev-styles']);
-    gulp.watch(scriptBase.src,['dev-scripts']);
+gulp.task('dev-watch', () => {
+    gulp.watch(styleBase.src, ['dev-styles']);
+    gulp.watch(scriptBase.src, ['dev-scripts']);
 });
-gulp.task('dev',['dev-connect','dev-styles','dev-scripts','dev-watch']);
+gulp.task('dev', ['dev-connect', 'dev-styles', 'dev-scripts', 'dev-watch']);
 
-gulp.task('clean',(cbk)=>{
-    del([base.dest+'**/*.*'],cbk)
+gulp.task('clean', (cbk) => {
+    del([base.dest, '.tmp'], {force: true},cbk);
 });
 
-gulp.task('usemin',()=>{
+gulp.task('usemin', ['build-scss'], () => {
     return gulp.src('src/index.html')
         .pipe(usemin({
-            style:[sass({outputStyle: 'compressed'}).on('error', sass.logError),autoprefix({
-                browsers:['> 1%','last 4 Android versions']
+            style: [autoprefix({
+                browsers: ['> 1%', 'last 4 Android versions']
             })],
-            es6:[babel(),uglify()]
+            es6: [babel(), uglify()]
         }))
         .pipe(htmlminify())
         .pipe(gulp.dest('build/'));
 });
 
 
-
-gulp.task('copy-assets',function () {
+gulp.task('copy-assets', function () {
     return gulp.src(['src/assets/**'], {base: 'src/assets'})
         .pipe(gulp.dest('build/assets/'));
 });
@@ -97,8 +102,7 @@ gulp.task('test-connect', function () {
 });
 
 
-
 gulp.task('build',
-    ['clean', 'usemin',  'copy-assets']
+    ['clean', 'usemin', 'copy-assets']
 );
-gulp.task('test',['build','test-connect']);
+gulp.task('test', ['build', 'test-connect']);
